b8f6ddffebb85c3eec1b5ee6cb5cf9606f7d47a3
Flo <fd.link@mci4me.at>
2026-01-03 14:22:07 +0100

    Room Editor View added

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,6 +21,11 @@ const router = createRouter({
       component: () => import('../views/RoomsView.vue'),
     },
     {
+    path: '/room-editor',
+    name: 'room-editor',
+    component: () => import('@/views/RoomEditorView.vue'),
+    },
+    {
       path: '/rooms/:id',
       name: 'room-detail',
       component: () => import('../views/RoomDetailView.vue'),
